3223c02c3c1c633d23c8868d2a8654ce1533f97d
valter2344 <valter.lonnqvist65@gmail.com>
2024-12-18 13:09:51 +0200

    Add files via upload

---
+++ csharpexam/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * C# Program to Accept a Number from the user and Display it 
+ * if it is Positive
+ */
+using System;
+class program
+{
+    public static void Main(string[] args)
+    {    //Code that propts the user to enter a number 
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        //Checks if positive
+        if (number > 0)
+        {     
+            Console.WriteLine("The number is positive");
+        }
+        //checks if number == 0
+        else if (number == 0)
+        {
+            Console.WriteLine("The number is 0");
+        }
+        //checks if number is negative
+        else
+        {
+            Console.WriteLine("The number is negative");
+        }
+        Console.ReadLine();
+    }
+}
---
+++ csharpexam/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays
+            string[] name = { "Alice", "John", "Mia" };
+            int[] num = { 5, 57, 87, 1, 150, 32, 14 };
+
+            //prints first array and adds Hello before every word
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine($" Hello {name[i]}");
